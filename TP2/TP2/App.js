ce8473469623dcb5ec636bebc59ba1ad27c5c79b
belgacem <benamierabelgacem@gmail.com>
2023-11-25 00:29:38 +0100

    feat :par l’affichage de nombre de clics sur un bouton.

--- TP2/TP2/App.js
+++ TP2/TP2/App.js
@@ -2,7 +2,7 @@
     * @description      : 
     * @author           : belgacem
     * @group            : 
-    * @created          : 25/11/2023 - 00:20:28
+    * @created          : 25/11/2023 - 00:27:58
     * 
     * MODIFICATION LOG
     * - Version         : 1.0.0
@@ -11,19 +11,19 @@
     * - Modification    : 
 **/
 import React, { useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native'; // Ajout de l'importation de Text
+import { View, StyleSheet, Text } from 'react-native';
 import CustomButton from './components/Ex2/CustomButton';
 
 const App = () => {
   const [message, setMessage] = useState('');
 
   const handleButtonClick = () => {
-    setMessage('Le bouton a été cliqué !');
+    setMessage(''); 
   };
 
   return (
     <View style={styles.container}>
-      <CustomButton onPress={handleButtonClick} />
+      <CustomButton onClick={handleButtonClick} />
       <View style={styles.messageContainer}>
         <Text>{message}</Text>
       </View>
